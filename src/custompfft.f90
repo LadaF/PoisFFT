b24f74781c43cc5f6f61b125b1ddb0f611b0d130
Vladimir Fuka <vladimir.fuka@gmail.com>
2015-11-19 12:04:13 +0000

    Forgotten import in the interface block.

--- src/custompfft.f90
+++ src/custompfft.f90
@@ -6,6 +6,7 @@ module PFFT
 
   interface
     subroutine pfft_plan_with_nthreads(nthreads) bind(C, name="pfft_plan_with_nthreads")
+      import
       integer(C_INT), value :: nthreads
     end subroutine
   end interface
